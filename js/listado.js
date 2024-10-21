7eb982bb9704e5b3bd549973277349a0ebdff0d6
MatyNG <164961137+MatyNG@users.noreply.github.com>
2024-10-22 01:37:25 +0200

    Add files via upload

---
+++ js/listado.js
@@ -0,0 +1,25 @@
+// listado.js
+
+// Variable que contiene el listado de equipos
+var equipos = [
+    "Arsenal",
+    "Aston Villa",
+    "Bournemouth",
+    "Brentford",
+    "Brighton & Hove Albion",
+    "Burnley",
+    "Chelsea",
+    "Crystal Palace",
+    "Everton",
+    "Fulham",
+    "Liverpool",
+    "Luton Town",
+    "Manchester City",
+    "Manchester United",
+    "Newcastle United",
+    "Nottingham Forest",
+    "Sheffield United",
+    "Tottenham Hotspur",
+    "West Ham United",
+    "Wolverhampton Wanderers"
+];
